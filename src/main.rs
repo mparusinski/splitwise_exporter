f20a32ac45ba007ad79403ef10a08470a08c24d5
Michal Parusinski <michal@parusinski.me>
2024-01-02 15:17:06 +0100

    Adding support for fetching expenses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
+use chrono::{Utc, DateTime, Duration};
+use splitwise::model::expenses::ListExpensesRequest;
+
+const FAMILY_GROUP_ID : i64 = 42513339;
+
+fn get_past_day_midnight() -> DateTime<Utc> {
+    let midnight = Utc::now().date().and_hms_opt(0,0,0).unwrap();
+    println!("Current date {:#?}", midnight);
+    midnight
+}
+
+fn build_expenses_request(group_id: i64) -> ListExpensesRequest {
+    let pd_midnight = get_past_day_midnight();
+    let nd_midnight = (pd_midnight + Duration::days(1)).date().and_hms_opt(0,0,0).unwrap();
+    ListExpensesRequest {
+        group_id: Some(group_id),
+        friend_id: None,
+        dated_after: Some(pd_midnight),
+        dated_before: Some(nd_midnight),
+        updated_after: None,
+        updated_before: None,
+        limit: None,
+        offset: None,
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let client = splitwise::client::Client::default();
-    let user = client.users().get_current_user().await.unwrap();
-    println!("Current user {:#?}", user)
+    // let groups = client.groups().list_groups().await.unwrap();
+    // println!("Current groups: {:#?}", groups);
+    let expenses = client.expenses().list_expenses(build_expenses_request(FAMILY_GROUP_ID)).await.unwrap();
+    println!("Current expenses: {:#?}", expenses);
 }
