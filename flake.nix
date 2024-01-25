c10ff5c03db7777154bbd0777b13212f6e91918b
Michal Parusinski <michal@parusinski.me>
2024-01-25 17:34:20 +0100

    Trying somethine else

--- flake.nix
+++ flake.nix
@@ -1,32 +1,27 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          pname = "splitwise_exporter";
-          src = ./.;
-          buildInputs = with pkgs; [ pkg-config openssl ];
-        };
-        packages.splitwise-exporter = naersk-lib.buildPackage {
-          pname = "splitwise_exporter";
-          src = ./.;
-          buildInputs = with pkgs; [ pkg-config openssl ];
-        };
-        devShell = with pkgs; mkShell {
-          buildInputs = [ 
-            cargo rustc rustfmt pre-commit rustPackages.clippy openssl pkg-config
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+  outputs = { self, nixpkgs, utils }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in utils.lib.eachDefaultSystem (system: {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [ cargo rustc pkg-config openssl ];
+    };
+    defaultPackage = pkgs.rustPlatform.buildRustPackage {
+      pname = "splitwise_exporter";
+      version = "0.1.0";
+      src = ./.;
+      buildInputs = with pkgs; [
+        pkg-config
+        openssl
+      ];
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+      cargoSha256 = "sha256-7esv0lYiYrwDu+27KP0Vh9FF+Cwdk/vKBVq/HjAdAmg=";
+    };
+  });
 }
