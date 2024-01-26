739dc014826d9488959220c70d3a53e37ccca99d
Michal Parusinski <michal@parusinski.me>
2024-01-26 15:58:44 +0100

    Definining default package

--- flake.nix
+++ flake.nix
@@ -8,20 +8,24 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-  in utils.lib.eachDefaultSystem (system: {
+  in utils.lib.eachDefaultSystem (system: 
+    let 
+      rustPkg = pkgs.rustPlatform.buildRustPackage {
+        pname = "splitwise_exporter";
+        version = "0.1.0";
+        src = ./.;
+        buildInputs = with pkgs; [
+          pkg-config
+          openssl
+        ];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        cargoSha256 = "sha256-7esv0lYiYrwDu+27KP0Vh9FF+Cwdk/vKBVq/HjAdAmg=";
+      };
+    in {
     devShells.default = pkgs.mkShell {
       packages = with pkgs; [ cargo rustc pkg-config openssl ];
     };
-    packages.splitwise-exporter = pkgs.rustPlatform.buildRustPackage {
-      pname = "splitwise_exporter";
-      version = "0.1.0";
-      src = ./.;
-      buildInputs = with pkgs; [
-        pkg-config
-        openssl
-      ];
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-      cargoSha256 = "sha256-7esv0lYiYrwDu+27KP0Vh9FF+Cwdk/vKBVq/HjAdAmg=";
-    };
+    packages.splitwise-exporter = rustPkg;
+    defaultPackage = rustPkg;
   });
 }
